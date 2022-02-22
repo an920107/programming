9edcf203c08fe8c4feca200635db4a9f4023e1e9
an920107 <an920107@gmail.com>
2022-02-22 18:55:08 +0800

    modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,93 +1,33 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <set>
 using namespace std;
- 
- 
-int main()
-{
-    long long int n,temp,sum=0,standard=0,cont=1,num[200000],min=1000000001,space=0;
-    set<long long int> num_able;
-    num_able.insert(0);
-    cin>>n;
-    for(long long int i=0;i<n;i++)
-    {
-        cin>>num[i];
-        if(num[i]<min)min=num[i];
-        sum+=num[i];
+
+void solve(string &str) {
+    stringstream sstream;
+    multiset<int> mst;
+    set<int> st;
+    int n = 0, tmp;
+    sstream << str;
+    while (sstream >> tmp) {
+        n ++;
+        st.insert(tmp);
+        mst.insert(tmp);
     }
-    if(min!=1)
-    {
-        cout<<"1";
-        return 0;
-    }
-    
-    sort(num,num+n);
-    long long int i;
-    for(i=0;i<n;i++)
-    {
-        temp=num[i];
-        if(space<cont&&cont>1)break;
-        if(cont>temp)
-        {
-            standard+=temp;
-        }
-        else
-        {
-            for(auto j=num_able.rbegin();j!=num_able.rend();j++)
-            {
-                num_able.insert(temp+*j);
-            }
-            for(long long int k=0;k<=temp&&k<=standard;k++)
-                num_able.insert(k);
-            while(num_able.count(cont)==1)cont++;
-        }
-
-
-        /*for(auto j=num_able.begin();j!=num_able.end();j++)
-        {
-            cout<<*j<<' ';
-        }
-        cout<<"temp="<<temp<<" standard="<<standard<<" cont="<<cont<<" space="<<space<<endl;
-        */
-    }
-
-    if(cont<temp&&cont>1)i--;
-    for(;i<n;i++)
-    {
-        temp=num[i];
-        if(cont>temp)
-        {
-            standard+=temp;
-        }
-        else
-        {
-            standard+=cont;
-        }
-
-        /*for(auto j=num_able.begin();j!=num_able.end();j++)
-        {
-            cout<<*j<<' ';
-        }
-        cout<<"temp="<<temp<<" standard="<<standard<<" cont="<<cont<<" space="<<space<<endl;
-        */
-    }
-
-
-
-
-
-
-
-
-
- 
-    for(long long int i=0;i<9999999999;i++)
-    {
-        if(num_able.count(i)==0)
-        {
-            cout<<i+standard;
-            return 0;
+    for (auto &elm : st) {
+        if (mst.count(elm) > n / 2) {
+            cout << elm << "\n";
+            return;
         }
     }
-    cout<<"Wrong ans";
+    cout << "NO\n";
+}
+
+int main() {
+    string str;
+    while (getline(cin, str)) {
+        solve(str);
+    }
     return 0;
 }
