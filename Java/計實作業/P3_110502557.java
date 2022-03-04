0e304977dcf52a4420d75c22a55147c8a70c21df
an920107 <an920107@gmail.com>
2022-03-04 21:59:11 +0800

    new file:   "Java/\350\250\210\345\257\246\344\275\234\346\245\255/P3_110502557.java"

---
+++ Java/計實作業/P3_110502557.java
@@ -0,0 +1,38 @@
+/**
+ * Practice 02
+ * Student Number: 110502557
+ * Course: 2022-CE1004-A
+ */
+
+public class P3_110502557 {
+    public static void main(String[] args) {
+        // 以下不要改
+        Student putin = new Student("Putin", 59);
+        System.out.println(putin.getName() + " before: " + putin.getScore());
+        putin.changeScore(40);
+        System.out.println(putin.getName() + " after: " + putin.getScore());
+//        putin.score = 100;
+    }
+}
+
+class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void changeScore(int newScore) {
+        score = newScore;
+    }
+}
