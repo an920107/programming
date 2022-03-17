0d7b3ce0285f3e5b1df10e46ee4061cf4dca425c
an920107 <an920107@gmail.com>
2022-03-17 23:25:48 +0800

    modified:   .gitignore 	modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   C++/test.cpp 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557.zip" 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557_1.jpg" 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557_1/Main.java" 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557_1/chino.jpg" 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557_2.jpg" 	new file:   "Java/\350\250\210\346\246\202\344\275\234\346\245\255/A5_110502557_2/Main.java"

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bcd(int m, int n) {
-    if (m < n) return bcd(n, m);
-    if (n == 0) return m;
-    return bcd(n, m - n);
-}
-
 int main() {
-    printf("%d", bcd(51, 17*16));
-    return 0;
+    cout << "" << "\n";
 }
