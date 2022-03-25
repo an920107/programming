64a7ab3317f55fc1fc0b790c11f0e933044466de
an920107 <an920107@gmail.com>
2022-03-25 17:52:11 +0800

    modified:   .vscode/settings.json 	new file:   "Java/\350\250\210\345\257\246\350\200\203\350\251\246/stu_110502557/E1.java" 	new file:   "Java/\350\250\210\345\257\246\350\200\203\350\251\246/stu_110502557/E2.java" 	new file:   "Java/\350\250\210\345\257\246\350\200\203\350\251\246/stu_110502557/stu_110502557.zip"

---
+++ Java/計實考試/stu_110502557/E1.java
@@ -0,0 +1,157 @@
+package stu_110502557;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class E1 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int command;
+        float multiply;
+        while (true) {
+            command = scanner.nextInt();
+            if (command != 1) break;
+
+            Pokemon enemy = new Pokemon(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat());
+            enemy.setAttributes(Pokemon.Attributes.FIRE);
+
+            Pokemon templatePokemon = new Pokemon(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat());
+            Stack<Pokemon> pokemons = new Stack<>();
+            int[] orders = new int[3];
+            for (int i = 0; i < 3; i ++) {
+                orders[i] = scanner.nextInt();
+            }
+            for (int i = 2; i >= 0; i --) {
+                pokemons.add(templatePokemon);
+                pokemons.lastElement().setAttributes(orders[i]);
+            }
+
+            int rounds = 0;
+            while (true) {
+
+                // ta's turn
+
+                rounds ++;
+
+                switch (pokemons.lastElement().getAttributes()) {
+                    case Pokemon.Attributes.GRASS:
+                        multiply = 1.5f;
+                        break;
+                    case Pokemon.Attributes.WATER:
+                        multiply = 0.5f;
+                        break;
+                    default:
+                        multiply = 1;
+                        break;
+                }
+
+                pokemons.lastElement().lossHp(enemy.canSpecialAttack() ? enemy.getSpc() : enemy.getAtk() * multiply);
+                enemy.addHp((enemy.canSpecialAttack() ? enemy.getSpc() : enemy.getAtk() * multiply) * 0.2f);
+                enemy.addPower(0.25f);
+
+                if (pokemons.lastElement().getHp() <= 0) {
+                    pokemons.pop();
+                    if (pokemons.isEmpty()) break;
+                }
+
+                // my turn
+
+                rounds ++;
+
+                switch (pokemons.lastElement().getAttributes()) {
+                    case Pokemon.Attributes.GRASS:
+                        multiply = 0.5f;
+                        break;
+                    case Pokemon.Attributes.WATER:
+                        multiply = 1.5f;
+                        break;
+                    default:
+                        multiply = 1;
+                        break;
+                }
+
+                enemy.lossHp(pokemons.lastElement().canSpecialAttack() ? pokemons.lastElement().getSpc() : pokemons.lastElement().getAtk() * multiply);
+                pokemons.lastElement().addPower(0.25f);
+
+                if (enemy.getHp() <= 0) break;
+            }
+
+            if (pokemons.isEmpty()) {
+                System.out.printf("You Lose! Using %d rounds!\n", rounds);
+            }
+            else {
+                System.out.printf("You Win! Using %d rounds!\n", rounds);
+            }
+        }
+    }
+}
+
+class Pokemon {
+    public static class Attributes {
+        final public static int GRASS = 1;
+        final public static int FIRE = 2;
+        final public static int WATER = 3;
+    }
+
+    private float hp;
+    private float atk;
+    private float power;
+    private float spc;
+    private int attributes;
+
+    public Pokemon(float hp, float atk, float power) {
+        this.hp = hp;
+        this.atk = atk;
+        this.power = power;
+        spc = 1;
+    }
+
+    public float getHp() { return hp; }
+    public float getAtk() { return atk; }
+    public float getPower() { return power; }
+    public float getSpc() { return spc; }
+    public int getAttributes() { return attributes; }
+
+    public void setAttributes(int attributes) {
+        switch (attributes) {
+            case Attributes.GRASS:
+                spc = atk;    
+                hp *= 1.2;
+                break;
+            case Attributes.FIRE:
+                spc = atk * 1.1f;
+                hp *= 0.8;
+                atk *= 1.2;
+                break;
+            case Attributes.WATER:
+                spc *= atk * 1.7f;
+                hp *= 0.9;
+                atk *= 0.9;
+                break;
+            default: break;
+        }
+        this.attributes = attributes;
+    }
+
+    public void addHp(float adding) {
+        hp += adding;
+    }
+
+    public void lossHp(float hurt) {
+        hp -= hurt;
+    }
+
+    public void addPower(float acc) {
+        if (power >= 1) {
+            power = 0;
+            return;
+        }
+        power += acc;
+    }
+
+    public boolean canSpecialAttack() {
+        return power >= 1;
+    }
+    
+}
