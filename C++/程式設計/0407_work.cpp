c10e87df7dbab5c11d834c1d018754b32a62e94c
an920107 <an920107@gmail.com>
2022-04-07 12:58:15 +0800

    modified:   0407_work.cpp

--- C++/程式設計/0407_work.cpp
+++ C++/程式設計/0407_work.cpp
@@ -2,21 +2,18 @@
 using namespace std;
 
 struct Node{
-    int data;  //資料成員 
-    struct Node *next; //鏈結成員，存放下一節點的指標 
-}; //typedef struct node Node; //將struct node 定義為Node型態 
+    int data;
+    struct Node *next;
+};
 
 Node* add(Node*, Node*);
 
 int main(int argc, char** argv) {
-    //宣告三個固定、不連續的位置給a、b、c 
-    //node a,b,c;   //宣告a,b,c為node型態的變數 
-    //node *ptr=&a; //宣告ptr，並將它指向節點a 
     struct Node a, b, c, d; 
     struct Node *ptr = &a;
     
-    a.data = 12;  //設定節點a的data成員為12 
-    a.next = &b;  //將節點a的next成員指向下一個節點，即b 
+    a.data = 12;
+    a.next = &b;
     b.data = 30;
     b.next = &c;
     c.data = 64;
@@ -25,11 +22,11 @@ int main(int argc, char** argv) {
     d.data = 24;
     
     cout << endl;
-    while(ptr != NULL) {                          //當ptr不是NULL時，則執行下列敘述 
-        cout << "adress=" << ptr << " ";             //印出節點位址 
-        cout << "data=" << ptr->data << " ";         //印出節點data 
-        cout << "next=" << ptr->next << endl;        //印出節點next
-        ptr = ptr->next;                       //將ptr指向下一個節點 
+    while(ptr != NULL) {
+        cout << "adress=" << ptr << " ";
+        cout << "data=" << ptr->data << " ";
+        cout << "next=" << ptr->next << endl;
+        ptr = ptr->next;
     } 
     
     system("pause");
