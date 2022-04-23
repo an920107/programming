7ab6f1eb4f9e8ec659d095455e5af49cc49ad8d3
an920107 <an920107@gmail.com>
2022-04-23 23:26:55 +0800

    modified:   C++/test.cpp 	new file:   C++/url_list.txt 	new file:   C++/url_list_new.txt 	modified:   Python/res.html 	modified:   Python/test.py

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void reader(const string str, set<string> &se) {
+    freopen(str.c_str(), "r", stdin);
+    string s;
+    while (cin >> s) {
+        cerr << s << '\n';
+        se.insert(s);
+    }
+    cin.clear();
+    fclose(stdin);
+}
+
 int main() {
-    
+    // FILE* file_all = fopen("url_list.txt", "r");
+    // FILE* file_new = fopen("url_list_new.txt", "r");
+    string line;
+    set<string> st;
+    reader("url_list.txt", st);
+    reader("url_list_new.txt", st);
+    freopen("url_list.txt", "w", stdout);
+    for (string str : st) {
+        cout << str << '\n';
+        cerr << str << '\n';
+    }
 }
--- Python/test.py
+++ Python/test.py
@@ -8,13 +8,14 @@ url = "https://bingx.com/en-us/traders/1026369846363258884"
 headers = {
     'user-agent':
         'Mozilla / 5.0(Windows NT 10.0; WOW64) '
-        'AppleWebKit / 537.36(KHTML, likeGecko) '
-        'Chrome / 53.0.2785.104Safari / 537.36Core / 1.53.4882.400QQBrowser / 9.7.13059.400'
+        # 'AppleWebKit / 537.36(KHTML, likeGecko)? '
+        # 'Chrome / 53.0.2785.104Safari / 537.36Core / 1.53.4882.400QQBrowser / 9.7.13059.400'
 }
-ua = UserAgent()
-scraper = cfscrape.create_scraper()
+# ua = UserAgent()
+# scraper = cfscrape.create_scraper()
 
-res = scraper.get(url, headers=headers)
-# file = open("res.html", "w")
-# file.write(res)
+# res = scraper.get(url, headers=headers)
+res = requests.get(url, headers=headers)
+file = open("res.html", "w")
+file.write(res.text)
 print(res.status_code)
