1d1711ff815e82210fa90e3660d04b4d2277a670
an920107 <an920107@gmail.com>
2022-05-26 22:08:43 +0800

    a12

--- Java/計概作業/A12_110502557/A12_1_110502557.java
+++ Java/計概作業/A12_110502557/A12_1_110502557.java
@@ -1,7 +1,17 @@
 package A12_110502557;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class A12_1_110502557 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        Document document = Jsoup.connect("https://www.csie.ncu.edu.tw/").get();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("A12_1_110502557.txt")))) {
+            writer.write(document.outerHtml());
+        }
     }
 }
