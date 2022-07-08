93ffb294dc8e80e83b8e89420892281f1e3fb1d3
an920107 <an920107@wsl>
2022-07-08 15:42:54 +0800

    modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,60 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct node{
-	int data;
-	struct node *next;
-};
-typedef struct node Node;
-
-void createQ();
-void addQ(int);
-void deleteQ();
-void printList();
-
-Node *Front, *Rear;
-
-int main(int argc, char** argv) {
-	
-	createQ();
-	addQ(5);
-	addQ(6);
-	addQ(7);
-	printList();
-	deleteQ();
-	printList();
-
-    system("pause");
+int main() {
+	int a;
+	cout << " Hi ! " << endl;
+	cin >> a; 
 	return 0;
-}
-
-void createQ() {
-	Front = Rear = (Node *)malloc(sizeof(Node));
-    Front->next = Rear->next = NULL;
-}
-
-void addQ(int item) {
-	Node *newnode = (Node *)malloc(sizeof(Node));
-	newnode->data = item;
-    newnode->next = NULL;
-    Front->next = newnode;
-    Rear = newnode;
-}
-
-void deleteQ() {
-	Node *temp = Front->next;
-    if (temp == NULL) return;
-    if (temp->next == NULL) Rear->next = Front;
-    Front->next = temp->next;
-    delete temp;
-}
-
-void printList() {
-	Node *temp;
-	temp = Front->next;
-	while (temp != NULL){
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
 }
