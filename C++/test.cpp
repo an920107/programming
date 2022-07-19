c628d6849c48695af1ce8d65c6bfd4d1e5311a26
an920107 <an920107@gmail.com>
2022-07-20 04:00:21 +0800

    update

--- C++/test.cpp
+++ C++/test.cpp
@@ -3,20 +3,6 @@ using namespace std;
 
 int main() {
 
-    srand(time(NULL));
-
-    string str = "";
-    for (char c = '0'; c <= '9'; c ++)
-        str += c;
-    for (char c = 'a'; c <= 'z'; c ++)
-        str += c;
-    for (char c = 'A'; c <= 'Z'; c ++)
-        str += c;
-    str += "!@#$%^&*/()-_=+";
-
-    for (int i = 0; i < 16; i ++)
-        cout << str[rand() % str.length()];
-    cout << endl;
 
     return 0;
 }
