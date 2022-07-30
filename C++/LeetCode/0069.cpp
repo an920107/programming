f1d1acfcdcf7002746e2b9b1d185fe2ca20de95a
an920107 <an920107@gmail.com>
2022-07-30 15:55:55 +0800

    modified:   C++/LeetCode/0069.cpp

--- C++/LeetCode/0069.cpp
+++ C++/LeetCode/0069.cpp
@@ -4,10 +4,8 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        for (long long i = 1; i <= x + 1; ++ i) {
-            if (i * i > (long long)(x)) return i - 1;
-        }
-        return 0;
+        int lower = 0;
+        int upper = 2147483647;
     }
 };
 
