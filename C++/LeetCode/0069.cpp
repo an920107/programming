c3130c79798eb66d9ba027e5c252991c8ee853db
an920107 <an920107@gmail.com>
2022-08-01 13:26:22 +0800

    modified:   C++/LeetCode/0069.cpp

--- C++/LeetCode/0069.cpp
+++ C++/LeetCode/0069.cpp
@@ -4,15 +4,28 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int lower = 0;
-        int upper = 2147483647;
+        if (x <= 1) return x;
+        int lower = 1;
+        int upper = 46341;
+        int mid = 23171;
+        while (true) {
+            if (upper == lower) return mid - 1;
+            int mul = mid * mid;
+            if (mul < x) lower = mid + 1;
+            else if (mul == x) return mid;
+            else upper = mid;
+            mid = (upper + lower) / 2;
+        }
+        return 46340;
     }
 };
 
 int main() {
     
     Solution sol;
-    cout << sol.mySqrt(2147395600) << endl;
+    for (int i = 0; i <= 100; i ++) {
+        printf("%-4d%-3d%-3d\n", i, sol.mySqrt(i), int(sqrt(i)));
+    }
 
     return 0;
 }
