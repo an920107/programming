f257625819019dce95ba39055f801d2f12e6997f
an920107 <an920107@gmail.com>
2022-09-18 15:13:55 +0800

    modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main() {
 
+    stringstream ss;
+    ss << "1+25";
+    char ch;
+    int i;
+    cout << ss.rdbuf()->in_avail() << '\n';
+    ss >> i;
+    cout << ss.rdbuf()->in_avail() << '\n';
+    ss >> ch;
+    cout << ss.rdbuf()->in_avail() << '\n';
+    ss >> i;
+    cout << ss.rdbuf()->in_avail() << '\n';
+    cout << i << ch;
 
     return 0;
 }
