7855fbf34225232d1a27624e9aa02e386978dc19
an920107 <an920107@gmail.com>
2022-09-28 10:28:55 +0800

    modified:   Python/test.py

--- Python/test.py
+++ Python/test.py
@@ -1,18 +1,7 @@
 score = int(input())
 
-<<<<<<< HEAD
-import math
-
-math.radians(90)
-math.sin(math.radians(90))
-
-
-
-# %%
-=======
 if score < 60:
     print("failed")
 else:
     print("pass")
     
->>>>>>> 7fd496962d5c6ee8e38204dde7f1a5d38ba70a1e
