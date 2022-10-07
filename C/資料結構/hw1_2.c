ddb6b0fca7435b89a0dcdc162edf3d4b1ababe5b
an920107 <an920107@gmail.com>
2022-10-08 05:23:14 +0800

    data structure

--- C/資料結構/hw1_2.c
+++ C/資料結構/hw1_2.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct stack {
     struct stack *next;
     char data;
 } stack;
 
-stack *s_push(stack *s, char c) {
+stack *init() {
+    stack *new_stack = (stack*)malloc(sizeof(stack));
+    new_stack->next = NULL;
+    new_stack->data = '\0';
+    return new_stack;
+}
+
+bool empty(stack *s) {
+    return s->next == NULL;
+}
+
+stack *push(stack *s, char c) {
     stack *new_s = (stack*)malloc(sizeof(stack));
     new_s->next = s;
     new_s->data = c;
     return new_s;
 }
 
-stack *s_pop(stack *s) {
+stack *pop(stack *s) {
+    if (empty(s))
+        return s;
     stack *new_s = s->next;
     free(s);
     return new_s;
@@ -32,18 +47,27 @@ int priority(char c) {
 }
 
 int main() {
-    stack *stk = (stack*)malloc(sizeof(stack));
-    stk->next = NULL;
-    stk->data = '\0';
-    
-
+    stack *str = init();
+    stack *opts = init();
+    stack *result = init();
     char input;
     while (input = getchar()) {
-        if (input == "\n") break;
-        if (!priority(input))
-        if (stk->next == NULL || priority(input) >= priority(stk->data))
-            stk = s_push(stk, input);
+        if (input == '\n') break;
+        str = push(str, input);
     }
-
+    for (; !empty(str); str = pop(str)) {
+        if (!priority(str->data)) // is number
+            result = push(result, str->data);
+        else {
+            for (; !empty(opts) && priority(str->data) < priority(opts->data); opts = pop(opts))
+                result = push(result, opts->data);
+            opts = push(opts, str->data);
+        }
+    }
+    for (; !empty(opts); opts = pop(opts))
+        result = push(result, opts->data);
+    for (; !empty(result); result = pop(result))
+        printf("%c", result->data);
+    printf("\n");
     return 0;
 }
