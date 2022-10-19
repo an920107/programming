79a859c8f2452f423f082e8cbf79d5d7f17efa39
an920107 <an920107@gmail.com>
2022-10-19 20:53:31 +0800

    modified:   C++/test.cpp 	modified:   C++/test3.cpp 	modified:   Python/test.ipynb

--- C++/test3.cpp
+++ C++/test3.cpp
@@ -92,7 +92,7 @@ int main() {
     Matrix<int> z(3, 3, {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
     Matrix<int> y(3, 1, {{1}, {2}, {3}});
 
-    cout << x * (z * y);
+    cout << x * z;
 
     return 0;
 }
