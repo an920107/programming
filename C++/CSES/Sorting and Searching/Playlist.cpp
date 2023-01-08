ff8ba3b21caf8b33c1547bd998cc5928bbc36fff
an920107 <an920107@gmail.com>
2023-01-08 16:02:36 +0800

    cses

---
+++ C++/CSES/Sorting and Searching/Playlist.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> &nums) {
+    
+    return 0;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i ++)
+        cin >> nums[i];
+
+    return 0;
+}
