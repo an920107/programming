3c7e60f43e0a0c1e422f6f91f3d787ef49fb97e5
an920107 <an920107@gmail.com>
2023-03-23 20:52:19 +0800

    hw

--- C++/test5.cpp
+++ C++/test5.cpp
@@ -1,36 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> &vec) {
-    int result = 1;
-    for (int start = 0; start < vec.size(); start ++) {
-        int index = start, current = 1;
-        bool state = true;
-        for (; index < vec.size() - 1; (++ index)) {
-            if (vec[index] > vec[index + 1] == state) {
-                result = max(result, (++ current));
-                state = !state;
-            }
-            else {
-                start = index;
-                break;
-            }
-        }
-    }
-    return result;
+void solve(vector<int> &vec) {
+
+    vector<vector<int>> dp(vec.size() + 1, vector<int>(vec.size() + 1));
+    
 }
 
 int main() {
+
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
 
     int n;
     cin >> n;
     vector<int> vec(n);
     for (int i = 0; i < n; i ++)
-        cin >> vec[i];
-
-    cout << solve(vec) << '\n';
+        cin >> vec[n];
+    solve(vec);
 
     return 0;
 }
-
-// + - + - + + + - + - +
