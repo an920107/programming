85bbcdb617e00d6a47bdf8c79dc90db1642ce8a9
an920107 <an920107@gmail.com>
2023-11-11 18:28:24 +0800

    compiler hw

--- .vscode\classpath/10127_ones.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        n = int(input())
-        m_str = "1"
-        while int(m_str) % n != 0:
-            m_str += "1"
-        print(len(m_str))
-    except:
-        break
