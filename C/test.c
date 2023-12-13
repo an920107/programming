9dedfef8d8aaeb3d51d101442a8d216cea0ba4f8
an920107 <an920107@gmail.com>
2023-12-13 14:19:54 +0800

    compiler hw3_2

--- C/test.c
+++ C/test.c
@@ -1,115 +1,65 @@
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX_SIZE 200000
-#define MAX_LINE 1024
-//#define INFINITE 1000000
+#include <string.h>
 
-void swap(int *x, int *y){
-    // 變數為address
-    int temp;
-    temp=*y;
-    *y=*x;
-    *x=temp;
+// struct definitions
+typedef struct node node_t;
+struct node {
+    node_t* last;
+    int value;
+};
+
+typedef struct stack stack_t;
+struct stack {
+    node_t* top;
+};
+
+typedef struct pair pair_t;
+struct pair {
+    int x, y;
+};
+
+// method definitions
+bool stack_is_empty(stack_t* s) { return s->top == NULL; }
+
+void stack_push(stack_t* s, int value) {
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->last = s->top;
+    new_node->value = value;
+    s->top = new_node;
 }
 
-void Merge(int arr[], int front, int mid, int end){
-    int left_index = front;
-    int right_index = mid+1;
-    int tempArr_size = end-front+1;
-    int tempArr[MAX_SIZE];
-    int tempArr_index = 0;
-    int arr_index;
-
-    while(left_index <= mid && right_index <= end){
-        if(arr[left_index] <= arr[right_index]){
-            tempArr[tempArr_index] = arr[left_index];
-            left_index++;
-        }
-        else{
-            tempArr[tempArr_index] = arr[right_index];
-            right_index++;
-        }
-        tempArr_index++;
-    }
-    if(left_index > mid){
-        while(right_index <= end){
-            tempArr[tempArr_index]=arr[right_index];
-            right_index++;
-            tempArr_index++;
-        }
-    }
-    if(right_index > end){
-        while(left_index <= mid){
-            tempArr[tempArr_index] = arr[left_index];
-            left_index++;
-            tempArr_index++;
-        }
-    }
-    arr_index = front;
-    for(tempArr_index = 0;tempArr_index < tempArr_size;tempArr_index++){
-        arr[arr_index] = tempArr[tempArr_index];
-        arr_index++;
-    }
-}
-void Merge_sort(int arr[], int front, int end){
-    if(front < end){
-        int mid = (front+end)/2;
-        Merge_sort(arr, front, mid);
-        Merge_sort(arr, mid+1, end);
-        Merge(arr, front, mid, end);
-        if(mid == 0){
-            printf("First pass: ");
-            for(int i=0;i<=20;i++){
-                if(i == 20){
-                    printf("\n");
-                    break;
-                }
-                printf("%d ", arr[i]);
-            }
-        }
-        if(mid == 1){
-            printf("Second pass: ");
-           for(int i=0;i<=20;i++){
-                if(i == 20){
-                    printf("\n");
-                    break;
-                }
-                printf("%d ", arr[i]);
-            } 
-        }
-    }
+void stack_pop(stack_t* s) {
+    if (stack_is_empty(s)) return;
+    s->top = s->top->last;
 }
 
-void Radix_sort(int arr[]){}
+int stack_top(stack_t* s) {
+    if (stack_is_empty(s)) return;
+    return s->top->value;
+}
 
-int main()
-{
-    int arr[MAX_SIZE];
-    char striline[MAX_LINE];
-    int front = 0;
-    int end = sizeof(arr) / sizeof(int);
-    // or int end = sizeof(a) / sizeof(int);
-    FILE *fptr;
-    fptr = fopen("sort的input file.txt", "r");
-    if(fptr == NULL){
-        printf("%s\n", "Failed.");
-        return -1;
-    }
-    int i=0;
-    while(!feof(fptr)){
-        fgets(striline, MAX_LINE, fptr);
-        //printf("%s", striline);
-        arr[i] = atoi(striline);
-        i++;
-    }
-    // int arr[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    printf("%d\n", end);
-    printf("\n");
-    Merge_sort(arr, 0, end);
-    /*printf("%d\n", arr[0]);
-    printf("%d\n", arr[1]);
-    printf("%d\n", arr[2000]);
-    printf("%d\n", arr[2001]);*/
-    printf("\n");
+pair_t get_params(stack_t* s) {
+    pair_t result;
+    if (stack_is_empty(s)) yyerror();
+    result.x = stack_top(s);
+    stack_pop(s);
+    if (stack_is_empty(s)) yyerror();
+    result.y = stack_top(s);
+    stack_pop(s);
+    return result;
+}
+
+int get_param(stack_t* s) {
+    if (stack_is_empty(s)) yyerror();
+    int result = stack_top(s);
+    stack_pop(s);
+    return result;
+}
+
+// initialization
+
+int main() {
+    strlen("123");
     return 0;
 }
